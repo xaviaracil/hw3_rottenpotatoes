9a828b7f2d17be653a5ec638ff2c094601c707b2
Xavi Aracil <xavi@xadsolutions.com>
2012-06-11 22:41:42 +0200

    Comment deleted

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -6,7 +6,6 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
     Movie.create! movie unless Movie.find :first, :conditions => movie
   end
-  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
