b9174381a93eebe09e44b39c06f2ef0b28aa3ed3
Xavi Aracil <xavi@xadsolutions.com>
2012-06-11 23:23:42 +0200

    Step definition created for: Given I check the following ratings: G, PG, R

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,4 +25,9 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s*/).each do |rating|
+    steps %Q{
+      When I #{'un' if uncheck}check "ratings[#{rating}]"
+    } 
+  end
 end
